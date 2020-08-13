a2ead4e94bae4acf0d9164781a33b31c276d3195
Alex Shulman <shulmanbusiness@gmail.com>
2020-08-12 20:14:14 -0400

    modify file structure, add beginning of hexagon grid

---
+++ Assets/Scripts/Level/WorldGrid.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGrid
+{
+    private GridTile[] m_gridArray;
+
+    public WorldGrid()
+    {
+
+    }
+
+    public void SetupSquareGrid()
+    {
+        int numGridTiles = Constants.GridSizeX * Constants.GridSizeY;
+        m_gridArray = new GridTile[numGridTiles];
+
+        for (int i = 0; i < numGridTiles; i++)
+        {
+            m_gridArray[i] = new GridTile();
+        }
+    }
+
+    public void SetupHexagonGrid()
+    {
+        int numGridTiles = 1;
+        for (int i = 3; i < Constants.GridDiameter; i += 2)
+        {
+            numGridTiles += 6 * ((i - 1) / 2);
+        }
+
+        m_gridArray = new GridTile[numGridTiles];
+
+        for (int i = 0; i < numGridTiles; i++)
+        {
+            m_gridArray[i] = new GridTile();
+        }
+    }
+}
---
+++ Assets/Scripts/Values/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants
+{
+    //Decide if using square or hexagon grid
+    public static bool IsSquareGrid = true;
+    
+    //Sizing for a "square" hexagon grid
+    public static int GridSizeX = 50;
+    public static int GridSizeY = 50;
+
+    //Sizing for a "hexagon" hexagon grid
+    public static int GridDiameter = 49;
+}
