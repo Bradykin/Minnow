3918e48bba977330251dbf6d4410e88eaec76742
Alex Shulman <shulmanbusiness@gmail.com>
2020-08-15 21:04:23 -0400

    grid movement testing

--- Assets/Scripts/Engine/Extensions/WorldGridTileExtensions.cs
+++ Assets/Scripts/Engine/Extensions/WorldGridTileExtensions.cs
@@ -6,7 +6,7 @@ namespace Game.Util
 {
     public static class WorldGridTileExtensions
     {
-        public static Vector2Int? Left(this WorldGridTile gridTile)
+        public static Vector2Int Left(this WorldGridTile gridTile)
         {
             Vector2Int currentPosition = gridTile.GridPosition;
 
@@ -15,10 +15,10 @@ namespace Game.Util
                 return currentPosition + Vector2Int.left;
             }
 
-            return null;
+            return currentPosition;
         }
 
-        public static Vector2Int? Right(this WorldGridTile gridTile)
+        public static Vector2Int Right(this WorldGridTile gridTile)
         {
             Vector2Int currentPosition = gridTile.GridPosition;
 
@@ -27,7 +27,68 @@ namespace Game.Util
                 return currentPosition + Vector2Int.right;
             }
 
-            return null;
+            return currentPosition;
+        }
+
+        public static Vector2Int UpLeft(this WorldGridTile gridTile)
+        {
+            Vector2Int currentPosition = gridTile.GridPosition;
+
+            if (!(currentPosition.y == Constants.GridSizeY - 1
+                || currentPosition.x == 0 && currentPosition.y % 2 == 0))
+            {
+                currentPosition += Vector2Int.up;
+                if (currentPosition.y % 2 == 1)
+                    currentPosition += Vector2Int.left;
+
+            }
+
+            return currentPosition;
+        }
+
+        public static Vector2Int UpRight(this WorldGridTile gridTile)
+        {
+            Vector2Int currentPosition = gridTile.GridPosition;
+
+            if (!(currentPosition.y == Constants.GridSizeY - 1
+               || currentPosition.x == Constants.GridSizeX - 1 && currentPosition.y % 2 == 1))
+            {
+                currentPosition += Vector2Int.up;
+                if (currentPosition.y % 2 == 0)
+                    currentPosition += Vector2Int.right;
+            }
+
+            return currentPosition;
+        }
+
+        public static Vector2Int DownLeft(this WorldGridTile gridTile)
+        {
+            Vector2Int currentPosition = gridTile.GridPosition;
+
+            if (!(currentPosition.y == 0
+                || currentPosition.x == 0 && currentPosition.y % 2 == 0))
+            {
+                currentPosition += Vector2Int.down;
+                if (currentPosition.y % 2 == 1)
+                    currentPosition += Vector2Int.left;
+            }
+
+            return currentPosition;
+        }
+
+        public static Vector2Int DownRight(this WorldGridTile gridTile)
+        {
+            Vector2Int currentPosition = gridTile.GridPosition;
+
+            if (!(currentPosition.y == 0
+               || currentPosition.x == Constants.GridSizeX - 1 && currentPosition.y % 2 == 1))
+            {
+                currentPosition += Vector2Int.down;
+                if (currentPosition.y % 2 == 0)
+                    currentPosition += Vector2Int.right;
+            }
+
+            return currentPosition;
         }
     }
 }
--- Assets/Scripts/Level/WorldGridManager.cs
+++ Assets/Scripts/Level/WorldGridManager.cs
@@ -9,9 +9,38 @@ public class WorldGridManager : MonoBehaviour, IReset
 
     private bool m_setup;
 
+    public GameObject m_testMovementObject;
+    private WorldGridTile m_currentTile;
+
     void Start()
     {
+
+    }
+
+    void Update()
+    {
+        if (!m_setup)
+            return;
         
+        if (Input.GetKeyDown(KeyCode.A))
+            m_currentTile = GetWorldGridTileAtPosition(m_currentTile.Left());
+
+        if (Input.GetKeyDown(KeyCode.D))
+            m_currentTile = GetWorldGridTileAtPosition(m_currentTile.Right());
+
+        if (Input.GetKeyDown(KeyCode.W))
+            m_currentTile = GetWorldGridTileAtPosition(m_currentTile.UpLeft());
+
+        if (Input.GetKeyDown(KeyCode.E))
+            m_currentTile = GetWorldGridTileAtPosition(m_currentTile.UpRight());
+
+        if (Input.GetKeyDown(KeyCode.Z))
+            m_currentTile = GetWorldGridTileAtPosition(m_currentTile.DownLeft());
+
+        if (Input.GetKeyDown(KeyCode.X))
+            m_currentTile = GetWorldGridTileAtPosition(m_currentTile.DownRight());
+
+        m_testMovementObject.transform.position = m_currentTile.transform.position;
     }
 
     public void Activate()
@@ -20,6 +49,9 @@ public class WorldGridManager : MonoBehaviour, IReset
         {
             SetupSquareGrid();
             m_setup = true;
+
+            m_currentTile = GetWorldGridTileAtPosition(3, 3);
+            m_testMovementObject.transform.position = m_currentTile.transform.position;
         }
     }
 
@@ -43,5 +75,15 @@ public class WorldGridManager : MonoBehaviour, IReset
             m_gridArray[i].Init(x, y);
             m_gridArray[i].transform.position = new Vector3((x + y * 0.5f - y / 2) * Constants.HexagonInnerRadius * 2.0f, y * Constants.HexagonOuterRadius * 1.5f, 0.0f);
         }
+    }
+
+    public WorldGridTile GetWorldGridTileAtPosition(int x, int y)
+    {
+        return m_gridArray[x + y * Constants.GridSizeX];
+    }
+
+    public WorldGridTile GetWorldGridTileAtPosition(Vector2Int position)
+    {
+        return GetWorldGridTileAtPosition(position.x, position.y);
     }
 }
