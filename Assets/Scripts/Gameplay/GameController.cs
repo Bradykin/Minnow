82f3b44c971a3466895d29a92dd467b8892f91cd
Bradykin <shulmanbusiness@gmail.com>
2020-08-22 18:47:02 -0400

    camera controller update

--- Assets/Scripts/Gameplay/GameController.cs
+++ Assets/Scripts/Gameplay/GameController.cs
@@ -19,6 +19,9 @@ public class GameController
         m_teamTurns = new List<ITurns>();
         m_teamTurns.Add(m_player);
         m_teamTurns.Add(new GameOpponent());
+
+        //TEMP: to start the turns. Should happen in a different way in future
+        BeginTurnSequence();
     }
 
     public void BeginTurnSequence()
@@ -28,13 +31,11 @@ public class GameController
 
     public void MoveToNextTurn()
     {
-        Debug.Log("End turn " + m_currentTurn);
         m_currentTurn.EndTurn();
         if (m_currentTurnIndex == m_teamTurns.Count - 1)
             m_currentTurnIndex = 0;
         else
             m_currentTurnIndex++;
         m_currentTurn.StartTurn();
-        Debug.Log("Start turn " + m_currentTurn);
     }
 }
