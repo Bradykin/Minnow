4cb148e4456be9791545b673a88f1f64923b7be5
nickmartino3 <nickmartino2011@hotmail.com>
2020-08-23 11:38:45 -0400

    Implement spinner enemy + fix naming on Seigebreaker file.

---
+++ Assets/Scripts/Gameplay/Content/Entities/Enemy/ContentSpinnerEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TODO: ashulman
+//For AI:
+//Charges at a spot
+//Attacks only once per turn, but hits all tiles around it
+public class ContentSpinnerEnemy : GameEntity
+{
+    public ContentSpinnerEnemy()
+    {
+        m_maxHealth = 7;
+        m_maxAP = 2;
+        m_apRegen = 2;
+        m_power = 3;
+        m_apToAttack = 0;
+
+        m_team = Team.Enemy;
+        m_rarity = GameRarity.Common;
+
+        m_name = "Spinner";
+        m_desc = "This guys spins and wins!  Hits all enemies around him";
+
+        LateInit();
+    }
+}
