9dd53593f7970177466c4c87164481ced45be884
nickmartino3 <nickmartino2011@hotmail.com>
2020-08-25 17:57:08 -0400

    Visual update to unit display

--- Assets/Scripts/Engine/Extensions/WorldTileExtensions.cs
+++ Assets/Scripts/Engine/Extensions/WorldTileExtensions.cs
@@ -15,8 +15,7 @@ namespace Game.Util
         {
             Vector3 screenPos = GetScreenPosition(worldTile);
 
-            screenPos.x += 0.5f;
-            screenPos.y -= 0.5f;
+            screenPos.y += 0.75f;
             screenPos.z = -1f;
 
             return screenPos;
@@ -26,7 +25,8 @@ namespace Game.Util
         {
             Vector3 screenPos = GetScreenPosition(worldTile);
 
-            screenPos.y += 0.5f;
+            screenPos.x += 0.6f;
+            screenPos.y -= 0.55f;
             screenPos.z = -1f;
 
             return screenPos;
@@ -36,8 +36,8 @@ namespace Game.Util
         {
             Vector3 screenPos = GetScreenPosition(worldTile);
 
-            screenPos.x -= 0.5f;
-            screenPos.y -= 0.5f;
+            screenPos.x -= 0.6f;
+            screenPos.y -= 0.55f;
             screenPos.z = -1f;
 
             return screenPos;
