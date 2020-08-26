eb06385cbf4bc5d112581e3e2254c6f87fb51346
Bradykin <shulmanbusiness@gmail.com>
2020-08-25 20:07:53 -0400

    call AISteps

--- Assets/Scripts/Gameplay/GameEnemyEntity.cs
+++ Assets/Scripts/Gameplay/GameEnemyEntity.cs
@@ -5,43 +5,17 @@ using UnityEngine;
 
 public class GameEnemyEntity : GameEntity, ITakeTurnAI
 {
-    public AIGameEnemyEntity AIGameEnemyEntity;
+    public AIGameEnemyEntity m_AIGameEnemyEntity;
 
     public GameEnemyEntity()
     {
-        AIGameEnemyEntity = new AIGameEnemyEntity(this);
+        m_AIGameEnemyEntity = new AIGameEnemyEntity(this);
     }
 
     //============================================================================================================//
 
     public void TakeTurn()
     {
-        List<WorldTile> tilesInRange = WorldGridManager.Instance.GetSurroundingTiles(WorldGridManager.Instance.GetWorldGridTileAtPosition(m_curTile.m_gridPosition), GetRange());
-
-        List<GameEntity> possibleEntityTargets = new List<GameEntity>();
-        List<GameBuildingBase> possibleBuildingTargets = new List<GameBuildingBase>();
-
-        foreach (var tile in tilesInRange)
-        {
-            if (tile.m_gameTile.m_building != null)
-            {
-
-            }
-
-            if (tile.m_gameTile.m_occupyingEntity != null && tile.m_gameTile.m_occupyingEntity.GetTeam() == Team.Enemy && CanHitEntity(tile.m_gameTile.m_occupyingEntity))
-            {
-
-            }
-        }
-
-        /*GameTile randomAdjacentTile = m_curTile.RandomAdjacentTile();
-
-        while (CanMoveTo(randomAdjacentTile))
-        {
-            MoveTo(randomAdjacentTile);
-            randomAdjacentTile = m_curTile.RandomAdjacentTile();
-
-            break;
-        }*/
+        m_AIGameEnemyEntity.TakeTurn();
     }
 }
