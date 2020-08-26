b15b2fed447bdd63836635d232afd16eee3d839f
Bradykin <shulmanbusiness@gmail.com>
2020-08-25 20:16:08 -0400

    rename file

--- Assets/Scripts/Gameplay/AI/AIScanTargetsInRangeStep.cs
+++ Assets/Scripts/Gameplay/AI/AIScanTargetsInRangeStep.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIScanTargetsStep : AIStep
+public class AIScanTargetsInRangeStep : AIStep
 {
     public override void TakeStep()
     {
