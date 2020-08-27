972e706ed6db395dc456202b201e10fe58547977
nickmartino3 <nickmartino2011@hotmail.com>
2020-08-27 18:49:41 -0400

    Background color change and camera bounds implemented

--- Assets/Scripts/UI/UICameraController.cs
+++ Assets/Scripts/UI/UICameraController.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class UICameraController : MonoBehaviour
 {
-    private float m_cameraSpeed = 6.0f;
+    private float m_cameraSpeed = 12.0f;
     private float m_cameraBound = 40f;
 
     private float m_zoomSpeed = 20.0f;
+
+    private float m_cameraLimitDown = 2.0f;
+    private float m_cameraLimitUp = 40.0f;
+    private float m_cameraLimitLeft = 15.0f;
+    private float m_cameraLimitRight = 40.0f;
 
     void Update()
     {
@@ -27,22 +32,34 @@ public class UICameraController : MonoBehaviour
 
         if (Input.mousePosition.x <= m_cameraBound && Input.mousePosition.x >= 0)
         {
-            movementVec.x -= m_cameraSpeed * Time.deltaTime;
+            if (transform.position.x > m_cameraLimitLeft)
+            {
+                movementVec.x -= m_cameraSpeed * Time.deltaTime;
+            }
         }
 
         if (Input.mousePosition.x >= Screen.width - m_cameraBound && Input.mousePosition.x <= Screen.width)
         {
-            movementVec.x += m_cameraSpeed * Time.deltaTime;
+            if (transform.position.x < m_cameraLimitRight)
+            {
+                movementVec.x += m_cameraSpeed * Time.deltaTime;
+            }
         }
 
         if (Input.mousePosition.y <= m_cameraBound && Input.mousePosition.y >= 0)
         {
-            movementVec.y -= m_cameraSpeed * Time.deltaTime;
+            if (transform.position.y > m_cameraLimitDown)
+            {
+                movementVec.y -= m_cameraSpeed * Time.deltaTime;
+            }
         }
 
         if (Input.mousePosition.y >= Screen.height - m_cameraBound && Input.mousePosition.y <= Screen.height)
         {
-            movementVec.y += m_cameraSpeed * Time.deltaTime;
+            if (transform.position.y < m_cameraLimitUp)
+            {
+                movementVec.y += m_cameraSpeed * Time.deltaTime;
+            }
         }
 
         transform.localPosition += movementVec;
