7fdbe422b6cc5b67523437f228853c5c4eddf4bb
nickmartino3 <nickmartino2011@hotmail.com>
2020-08-27 18:52:30 -0400

    Fix for HUD jittering when moving camera

--- Assets/Scripts/UI/UIHUDController.cs
+++ Assets/Scripts/UI/UIHUDController.cs
@@ -6,6 +6,6 @@ public class UIHUDController : MonoBehaviour
 {
     void Update()
     {
-        transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, -2.0f);
+        //transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, -2.0f);
     }
 }
