7af3d4af393f82403ea042b0a3a3767844fb1a64
Bradykin <shulmanbusiness@gmail.com>
2020-09-05 22:55:06 -0400

    playing with jsons

--- Assets/Scripts/Engine/JsonDataTypes/JsonGameTileData.cs
+++ Assets/Scripts/Engine/JsonDataTypes/JsonGameTileData.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JsonGameTileData
+public struct JsonGameTileData
 {
     public Vector2Int m_gridPosition;
+    public JsonGameEntityData m_gameEntityData;
+    public JsonGameBuildingData m_gameBuildingData;
+    public JsonGameTerrainData m_gameTerrainData;
+    public JsonGameEventData m_gameEventData;
 }
---
+++ Assets/Scripts/Engine/JsonDataTypes/JsonGridData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct JsonGridData
+{
+    public int m_gridSizeX;
+    public int m_gridSizeY;
+    public List<JsonGameTileData> m_jsonTileData;
+}
