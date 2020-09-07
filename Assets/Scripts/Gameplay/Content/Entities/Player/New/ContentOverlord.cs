73b183633d36f2466b05107bf41dfa113881f289
nickmartino3 <nickmartino2011@hotmail.com>
2020-09-07 12:51:54 -0400

    More boilerplate for big entity update

--- Assets/Scripts/Gameplay/Content/Entities/Player/New/ContentOverlord.cs
+++ Assets/Scripts/Gameplay/Content/Entities/Player/New/ContentOverlord.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class ContentOverlord : GameEntity
 {
+    public ContentOverlord()
+    {
+        m_maxHealth = 8;
+        m_maxAP = 4;
+        m_apRegen = 3;
+        m_power = 4;
 
+        m_team = Team.Player;
+        m_rarity = GameRarity.Common;
+
+        m_name = "Overlord";
+        m_typeline = Typeline.Monster;
+        m_icon = UIHelper.GetIconEntity(m_name);
+
+        LateInit();
+    }
 }
--- Assets/Scripts/Gameplay/Content/Entities/Player/New/ContentRaptor.cs
+++ Assets/Scripts/Gameplay/Content/Entities/Player/New/ContentRaptor.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class ContentRaptor : GameEntity
 {
+    public ContentRaptor()
+    {
+        m_maxHealth = 8;
+        m_maxAP = 4;
+        m_apRegen = 3;
+        m_power = 4;
 
+        m_team = Team.Player;
+        m_rarity = GameRarity.Common;
+
+        m_name = "Raptor";
+        m_typeline = Typeline.Monster;
+        m_icon = UIHelper.GetIconEntity(m_name);
+
+        LateInit();
+    }
 }
