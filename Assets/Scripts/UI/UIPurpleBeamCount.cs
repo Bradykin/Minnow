f751ffebf2d9c6c52123010a062aee91ed669a17
nickmartino3 <nickmartino2011@hotmail.com>
2020-09-08 12:47:49 -0400

    Add purple beam UI

---
+++ Assets/Scripts/UI/UIPurpleBeamCount.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPurpleBeamCount : WorldElementBase
+{
+    public Text m_valText;
+
+    public SpriteRenderer m_tintRenderer;
+    public GameObject m_holder;
+
+    void Update()
+    {
+        GamePlayer player = GameHelper.GetPlayer();
+
+        if (player == null)
+        {
+            return;
+        }
+
+        if (Globals.m_purpleBeamCount == 0)
+        {
+            m_holder.SetActive(false);
+        }
+        else
+        {
+            m_holder.SetActive(true);
+            m_valText.text = "" + Globals.m_purpleBeamCount;
+        }
+    }
+
+    void OnMouseOver()
+    {
+        UIHelper.SetValidTintColor(m_tintRenderer, true);
+        Globals.m_canScroll = false;
+    }
+
+    void OnMouseExit()
+    {
+        UIHelper.SetDefaultTintColor(m_tintRenderer);
+        Globals.m_canScroll = true;
+    }
+
+    public override void HandleTooltip()
+    {
+        UITooltipController.Instance.AddTooltipToStack(UIHelper.CreateSimpleTooltip("Purple Beam Count", "This is your purple beam count.  Some cards add to it, some cards spend it."));
+    }
+}
