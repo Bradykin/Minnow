7026cc9f387384ea3a5b06e014fa643875f1d230
Bradykin <shulmanbusiness@gmail.com>
2020-09-25 22:21:16 -0400

    buff ass contract

--- Assets/Scripts/Gameplay/Content/Cards/Spells/ContentAssassinationContractCard.cs
+++ Assets/Scripts/Gameplay/Content/Cards/Spells/ContentAssassinationContractCard.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ContentAssassinationContractCard : GameCardSpellBase
 {
+    private int m_shivAmount = 3;
+    
     public ContentAssassinationContractCard()
     {
         m_name = "Assassination Contract";
-        m_desc = "Add two shivs to your hand.";
+        m_desc = "Add " + m_shivAmount + " shivs to your hand.";
         m_playDesc = "I'll take that mark...";
         m_targetType = Target.None;
         m_cost = 2;
@@ -32,7 +34,9 @@ public class ContentAssassinationContractCard : GameCardSpellBase
 
         base.PlayCard();
 
-        player.AddCardToHand(new ContentShivCard(), false);
-        player.AddCardToHand(new ContentShivCard(), false);
+        for (int i = 0; i < m_shivAmount; i++)
+        {
+            player.AddCardToHand(new ContentShivCard(), false);
+        }
     }
 }
