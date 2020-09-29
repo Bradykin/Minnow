a19f19f252901c33f2701112963f6417cf214e87
nickmartino3 <nickmartino2011@hotmail.com>
2020-09-29 19:49:17 -0400

    Adjust angry birds

--- Assets/Scripts/Gameplay/Content/Entities/Enemy/ContentAngryBirdEnemy.cs
+++ Assets/Scripts/Gameplay/Content/Entities/Enemy/ContentAngryBirdEnemy.cs
@@ -8,9 +8,9 @@ public class ContentAngryBirdEnemy : GameEnemyEntity
     public ContentAngryBirdEnemy(GameOpponent gameOpponent) : base(gameOpponent)
     {
         m_maxHealth = 4;
-        m_maxAP = 8;
-        m_apRegen = 5;
-        m_power = 2;
+        m_maxAP = 6;
+        m_apRegen = 4;
+        m_power = 3;
 
         m_team = Team.Enemy;
         m_rarity = GameRarity.Common;
