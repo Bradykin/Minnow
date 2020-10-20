455fdeb0fc18f37fb2c3ccb193b9a698cc396d2c
Bradykin <shulmanbusiness@gmail.com>
2020-10-20 18:10:27 -0400

    set water tile back to previous

---
+++ Assets/Scripts/Gameplay/Content/Terrain/Basic Terrain/ContentLakeTerrain.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContentLakeTerrain : GameTerrainBase
+{
+    public ContentLakeTerrain()
+    {
+        m_damageReduction = Constants.WaterDamageReduction;
+        m_costToPass = Constants.WaterMovementCost;
+        m_isPassable = false;
+
+        m_name = "Lake";
+        m_desc = GenerateDescription();
+        m_maxTerrainImageNumber = 4;
+        m_terrainImageNumber = Random.Range(1, m_maxTerrainImageNumber + 1);
+
+        m_isWater = true;
+
+        LateInit();
+    }
+}
+
