0cb8ba75ebf15382f83f2209809d9bb07c88332c
Bradykin <shulmanbusiness@gmail.com>
2020-10-26 16:05:43 -0400

    remove commented out code

--- Assets/Scripts/UI/UIBorderUnit.cs
+++ Assets/Scripts/UI/UIBorderUnit.cs
@@ -38,54 +38,8 @@ public class UIBorderUnit : MonoBehaviour
     private void UpdatePosition()
     {
         Vector3 unitWorldToScreen = Camera.main.WorldToScreenPoint(m_unit.transform.position);
-        //Vector3 centerScreenPoint = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0));
-
         Vector2 clampedUnitWorldToScreen = new Vector2(Mathf.Clamp(unitWorldToScreen.x, m_rectTransform.rect.width / 2, UIHUDController.Instance.m_rectTransform.rect.width - (m_rectTransform.rect.width / 2)), 
                                                        Mathf.Clamp(unitWorldToScreen.y, m_rectTransform.rect.height / 2, UIHUDController.Instance.m_rectTransform.rect.height - (m_rectTransform.rect.height / 2)));
         m_rectTransform.anchoredPosition = clampedUnitWorldToScreen;
-        //m_rectTransform.anchoredPosition = Camera.main.WorldToScreenPoint(m_unit.transform.position);
-
-
-        /*Vector3 pos = Camera.main.transform.position;
-        Vector3 dir = (m_unit.transform.position - Camera.main.transform.position);
-        Vector3 normalizedDir = new Vector3(dir.x, dir.y, transform.position.z).normalized;
-
-        float leftBorder = 0.0f;
-        float rightBorder = Screen.width;
-        float topBorder = 0.0f;
-        float botBorder = Screen.height;
-
-        Vector3 screenPointPos = Camera.main.ViewportToScreenPoint(new Vector3(Screen.width * normalizedDir.x, Screen.height * normalizedDir.y, normalizedDir.z));
-        screenPointPos.z = 0;
-        gameObject.GetComponent<RectTransform>().position = screenPointPos;
-
-        m_rectTransform.anchoredPosition = new Vector2(Screen.width / 4, Screen.height / 4);
-
-        /*print("m_entiy World Pos: " + m_unit.transform.position);
-        print("m_entiy Screen Pos: " + Camera.main.WorldToScreenPoint(m_unit.transform.position));
-        print("m_entiy Viewport Pos: " + Camera.main.WorldToViewportPoint(m_unit.transform.position));
-
-        Vector3 unitScreenPos = Camera.main.WorldToScreenPoint(m_unit.transform.position);
-        float dist = (Camera.main.WorldToScreenPoint(m_unit.transform.position) - Camera.main.transform.position).z;
-        float leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).x;
-        float rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).x;
-        float topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).y;
-        float botBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, dist)).y;
-
-        gameObject.transform.position = new Vector3(Mathf.Clamp(unitScreenPos.x, leftBorder, rightBorder), Mathf.Clamp(unitScreenPos.y, topBorder, botBorder), unitScreenPos.z);*/
     }
 }
-
-/*
- *     private void UpdatePosition()
-    {
-        float leftBorder = 0.0f;
-        float rightBorder = Screen.width;
-        float topBorder = 0.0f;
-        float botBorder = Screen.height;
-
-        Vector3 worldUnitScreenPos = Camera.main.WorldToScreenPoint(m_unit.gameObject.transform.position);
-
-        gameObject.transform.position = new Vector3(Mathf.Clamp(worldUnitScreenPos.x, leftBorder, rightBorder), Mathf.Clamp(worldUnitScreenPos.y, topBorder, botBorder), gameObject.transform.position.z);
-    }
- */
