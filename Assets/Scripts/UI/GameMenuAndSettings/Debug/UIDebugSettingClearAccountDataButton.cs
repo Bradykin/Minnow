236bebe71865471a996d05a0da2d57678e344f69
nickmartino3 <nickmartino2011@hotmail.com>
2020-11-06 22:13:27 -0500

    Add clear player save data

---
+++ Assets/Scripts/UI/GameMenuAndSettings/Debug/UIDebugSettingClearAccountDataButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIDebugSettingClearAccountDataButton : UIElementBase
+        , IPointerClickHandler
+{
+    void Start()
+    {
+        if (!Constants.CheatsOn)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //Alex, clear account data here
+
+        AudioSFXController.Instance.PlaySFX(AudioHelper.UIClick);
+    }
+
+    public override void HandleTooltip()
+    {
+        //Left as stub
+    }
+}
