034f3cb3bc74060f1e915e9b65e1a9b3c68c1016
Bradykin <shulmanbusiness@gmail.com>
2020-11-12 08:26:09 -0500

    work on immortals spawning formula

--- Assets/Scripts/Gameplay/Content/Maps/ContentMountainPassMap.cs
+++ Assets/Scripts/Gameplay/Content/Maps/ContentMountainPassMap.cs
@@ -24,45 +24,75 @@ public class ContentMountainPassMap : GameMap
         immortalEnemyUnits.Add(new ContentImmortalBladeEnemy(null));
         immortalEnemyUnits.Add(new ContentImmortalBowEnemy(null));
         immortalEnemyUnits.Add(new ContentImmortalBannerEnemy(null));
+        List<GameEnemyUnit> activeBossUnits = GameHelper.GetGameController().m_activeBossUnits;
 
-        if (GetFogSpawningActive())
+
+        bool allSpawned = true;
+        for (int k = 0; k < immortalEnemyUnits.Count; k++)
         {
-            if (GameHelper.GetGameController().m_currentTurnNumber >= (gameOpponent.m_eliteSpawnWaveModifier + Constants.SpawnEliteTurn))
+            bool immortalIsSpawned = false;
+            for (int i = 0; i < activeBossUnits.Count; i++)
             {
-                GameEnemyUnit gameEnemyUnit = GameUnitFactory.GetRandomEliteEnemy(gameOpponent);
-                gameOpponent.TryForceSpawnAtEdgeOfFog(gameEnemyUnit, tilesAtFogEdge);
-                return true;
-            }
-        }
-        else
-        {
-            for (int i = 0; i < gameOpponent.m_spawnPoints.Count; i++)
-            {
-                GameSpawnPoint temp = gameOpponent.m_spawnPoints[i];
-                int randomIndex = UnityEngine.Random.Range(i, gameOpponent.m_spawnPoints.Count);
-                gameOpponent.m_spawnPoints[i] = gameOpponent.m_spawnPoints[randomIndex];
-                gameOpponent.m_spawnPoints[randomIndex] = temp;
+                if (activeBossUnits[i].GetType() == immortalEnemyUnits[k].GetType())
+                {
+                    immortalIsSpawned = true;
+                    break;
+                }
             }
 
-            if (GameHelper.GetGameController().m_currentTurnNumber >= (gameOpponent.m_eliteSpawnWaveModifier + Constants.SpawnEliteTurn))
+            if (immortalIsSpawned)
             {
-                GameEnemyUnit gameEnemyUnit = GameUnitFactory.GetRandomEliteEnemy(gameOpponent);
+                continue;
+            }
+
+            if (GetFogSpawningActive())
+            {
+                if (GameHelper.GetGameController().m_currentTurnNumber >= (gameOpponent.m_eliteSpawnWaveModifier + Constants.SpawnEliteTurn))
+                {
+                    GameEnemyUnit gameEnemyUnit = GameUnitFactory.GetEnemyUnitClone(immortalEnemyUnits[k]);
+                    if (!gameOpponent.TryForceSpawnAtEdgeOfFog(gameEnemyUnit, tilesAtFogEdge))
+                    {
+                        allSpawned = false;
+                    }
+                }
+            }
+            else
+            {
                 for (int i = 0; i < gameOpponent.m_spawnPoints.Count; i++)
                 {
-                    if (!gameOpponent.m_spawnPoints[i].m_tile.IsPassable(gameEnemyUnit, false))
+                    GameSpawnPoint temp = gameOpponent.m_spawnPoints[i];
+                    int randomIndex = UnityEngine.Random.Range(i, gameOpponent.m_spawnPoints.Count);
+                    gameOpponent.m_spawnPoints[i] = gameOpponent.m_spawnPoints[randomIndex];
+                    gameOpponent.m_spawnPoints[randomIndex] = temp;
+                }
+
+                if (GameHelper.GetGameController().m_currentTurnNumber >= (gameOpponent.m_eliteSpawnWaveModifier + Constants.SpawnEliteTurn))
+                {
+                    GameEnemyUnit gameEnemyUnit = GameUnitFactory.GetEnemyUnitClone(immortalEnemyUnits[k]);
+                    bool spawnedUnit = false;
+                    for (int i = 0; i < gameOpponent.m_spawnPoints.Count; i++)
                     {
-                        continue;
+                        if (!gameOpponent.m_spawnPoints[i].m_tile.IsPassable(gameEnemyUnit, false))
+                        {
+                            continue;
+                        }
+
+                        if (gameOpponent.TryForceSpawnAtSpawnPoint(gameEnemyUnit, gameOpponent.m_spawnPoints[i]))
+                        {
+                            spawnedUnit = true;
+                            break;
+                        }
                     }
 
-                    if (gameOpponent.TryForceSpawnAtSpawnPoint(gameEnemyUnit, gameOpponent.m_spawnPoints[i]))
+                    if (!spawnedUnit)
                     {
-                        return true;
+                        allSpawned = false;
                     }
                 }
             }
         }
 
-        return false;
+        return allSpawned;
     }
 
     public override bool TrySpawnBoss(List<GameTile> tilesAtFogEdge)
