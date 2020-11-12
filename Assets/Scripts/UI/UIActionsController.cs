67c54662a42e51fbbb84eef8f95ae94c8c08615b
nickmartino3 <nickmartino2011@hotmail.com>
2020-11-12 10:56:43 -0500

    Update actions UI

---
+++ Assets/Scripts/UI/UIActionsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIActionsController : UIElementBase
+{
+    public Text m_countText;
+
+    void Update()
+    {
+        GamePlayer player = GameHelper.GetPlayer();
+
+        m_countText.text = player.GetCurActions() + "/" + player.GetMaxActions();
+    }
+
+    public override void HandleTooltip()
+    {
+        UITooltipController.Instance.AddTooltipToStack(UIHelper.CreateSimpleTooltip("Actions", "Use these action points to take various actions during the intermission phase!"));
+    }
+}
