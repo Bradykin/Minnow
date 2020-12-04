d5ee9bb71da7faddcbe35f37de0445e8d727e52f
Bradykin <shulmanbusiness@gmail.com>
2020-12-03 19:15:21 -0500

    Updates to snowmelt

--- Assets/Scripts/Gameplay/Content/Maps/ContentSnowmeltMap.cs
+++ Assets/Scripts/Gameplay/Content/Maps/ContentSnowmeltMap.cs
@@ -42,4 +42,103 @@ public class ContentSnowmeltMap : GameMap
     {
         
     }
+
+    protected override void FillSpawnPool()
+    {
+        m_totalEnemiesOnMap.Add(new ContentChillflameBeastEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentFrostGiantEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentFrozenGuardianEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentFrozenImpEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentGreatFrostlizardEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentGriffonEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentIceWurmEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentLancerEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentLordOfWinterEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentOrcEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentOrcShamanEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentPolarWarriorEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentSabertoothWyvernEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentSnowprowlerEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentValgulaEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentYetiEnemy(null));
+
+        m_totalEnemiesOnMap.Add(new ContentHellhoundEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentBurningMonstrosityEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentPhoenixEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentLavaHellionEnemy(null));
+        m_totalEnemiesOnMap.Add(new ContentDemonMagicianEnemy(null));
+
+        //--------------------------------------------------------------------------------------------------------//
+
+        List<GameSpawnPoolData> defaultSpawnPoolData = new List<GameSpawnPoolData>();
+        //Wave 1
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentLancerEnemy(null), 1, 1, 1));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentValgulaEnemy(null), 1, 2, 0.66f));
+
+        //Wave 2
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentFrozenImpEnemy(null), 2, 1, 0.75f));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentLancerEnemy(null), 2, 1, 1));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentSnowprowlerEnemy(null), 2, 1, 1));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentValgulaEnemy(null), 2, 1, 1));
+
+        //Wave 3
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentFrozenImpEnemy(null), 3, 1, 0.5f));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentGriffonEnemy(null), 3, 1, 1));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentLancerEnemy(null), 3, 1, 1));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentSnowprowlerEnemy(null), 3, 1, 1));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentPolarWarriorEnemy(null), 3, 1, 1));
+
+        //Wave 4
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentChillflameBeastEnemy(null), 4, 1, 1));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentFrozenImpEnemy(null), 4, 0.5f, 0.33f));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentGriffonEnemy(null), 4, 1, 1));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentIceWurmEnemy(null), 4, 1.5f, 0.75f));
+
+        //Wave 5
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentChillflameBeastEnemy(null), 5, 1, 1));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentFrozenImpEnemy(null), 5, 0.5f, 0.33f));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentGreatFrostlizardEnemy(null), 5, 1, 1));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentIceWurmEnemy(null), 5, 1, 1));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentSabertoothWyvernEnemy(null), 5, 1, 1));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentYetiEnemy(null), 5, 1, 1));
+
+        //Wave 6
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentGreatFrostlizardEnemy(null), 6, 1, 1));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentIceWurmEnemy(null), 6, 1, 1));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentSabertoothWyvernEnemy(null), 6, 1, 1));
+        defaultSpawnPoolData.Add(new GameSpawnPoolData(new ContentYetiEnemy(null), 6, 1, 1));
+
+        //--------------------------------------------------------------------------------------------------------//
+
+        List<GameSpawnPoolData> volcanoIslandSpawnPoolData = new List<GameSpawnPoolData>();
+
+        //Wave 1
+
+        //Wave 2
+        volcanoIslandSpawnPoolData.Add(new GameSpawnPoolData(new ContentHellhoundEnemy(null), 2, 1.5f, 0.66f));
+
+        //Wave 3
+        volcanoIslandSpawnPoolData.Add(new GameSpawnPoolData(new ContentHellhoundEnemy(null), 3, 1, 1));
+
+        //Wave 4
+        volcanoIslandSpawnPoolData.Add(new GameSpawnPoolData(new ContentChillflameBeastEnemy(null), 4, 1, 0.25f));
+        volcanoIslandSpawnPoolData.Add(new GameSpawnPoolData(new ContentHellhoundEnemy(null), 4, 1, 1));
+
+        //Wave 5
+        volcanoIslandSpawnPoolData.Add(new GameSpawnPoolData(new ContentChillflameBeastEnemy(null), 5, 1, 1));
+        volcanoIslandSpawnPoolData.Add(new GameSpawnPoolData(new ContentDemonMagicianEnemy(null), 5, 1, 1));
+        volcanoIslandSpawnPoolData.Add(new GameSpawnPoolData(new ContentLavaHellionEnemy(null), 5, 1.5f, 1));
+        volcanoIslandSpawnPoolData.Add(new GameSpawnPoolData(new ContentPhoenixEnemy(null), 5, 1.5f, 1));
+
+        //Wave 6
+        volcanoIslandSpawnPoolData.Add(new GameSpawnPoolData(new ContentChillflameBeastEnemy(null), 6, 1, 1));
+        volcanoIslandSpawnPoolData.Add(new GameSpawnPoolData(new ContentDemonMagicianEnemy(null), 6, 1, 1));
+        volcanoIslandSpawnPoolData.Add(new GameSpawnPoolData(new ContentLavaHellionEnemy(null), 6, 1.5f, 1));
+        volcanoIslandSpawnPoolData.Add(new GameSpawnPoolData(new ContentPhoenixEnemy(null), 6, 1.5f, 1));
+
+        //--------------------------------------------------------------------------------------------------------//
+
+        m_defaultSpawnPool = new GameSpawnPool(defaultSpawnPoolData);
+        m_spawnPointSpawnPools.Add(new GameSpawnPool(volcanoIslandSpawnPoolData));
+    }
 }
