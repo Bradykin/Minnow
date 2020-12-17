42909c50e3692ec9ba0c35528c6316f801f1f4b5
nickmartino3 <nickmartino2011@hotmail.com>
2020-12-17 16:08:55 -0500

    Implemnet Magic Power UI

---
+++ Assets/Scripts/UI/UIMagicPowerCounter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class UIMagicPowerCounter : UIElementBase
+{
+    public TMP_Text m_countText;
+    public GameObject m_holder;
+
+    void Start()
+    {
+        m_stopScrolling = true;
+    }
+
+    void Update()
+    {
+        if (GameHelper.GetPlayer().GetMagicPower() == 0)
+        {
+            m_holder.SetActive(false);
+            return;
+        }
+        else
+        {
+            m_holder.SetActive(true);
+        }
+
+        m_countText.text = $"{GameHelper.GetPlayer().GetMagicPower()}";
+    }
+
+    public override void HandleTooltip()
+    {
+        UITooltipController.Instance.AddTooltipToStack(UIHelper.CreateSimpleTooltip("<b>Magic Power</b>", "This is your current <b>Magic Power</b>! This affects the power of various spells you cast!."));
+    }
+}
