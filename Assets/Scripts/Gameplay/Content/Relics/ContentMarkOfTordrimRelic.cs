c67964890f57b36142066ae1c894a9e16dd2f681
nickmartino3 <nickmartino2011@hotmail.com>
2020-12-22 17:06:06 -0500

    Mark of Tordrim text adjustment

--- Assets/Scripts/Gameplay/Content/Relics/ContentMarkOfTordrimRelic.cs
+++ Assets/Scripts/Gameplay/Content/Relics/ContentMarkOfTordrimRelic.cs
@@ -7,7 +7,7 @@ public class ContentMarkOfTordrimRelic : GameRelic
     public ContentMarkOfTordrimRelic()
     {
         m_name = "Mark of Tordrim";
-        m_desc = "When you summon a unit, give it a random ability.";
+        m_desc = "When you summon a unit, give it a random positive ability.";
         m_rarity = GameRarity.Rare;
 
         LateInit();
