6cbfcd5659ed2a430b57e331d2657aaaff348030
nickmartino3 <nickmartino2011@hotmail.com>
2020-12-30 11:25:14 -0500

    Confirmation Controller Implemented Added confirm to ending turn

---
+++ Assets/Scripts/UI/UIConfirmationAccept.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIConfirmationAccept : UIElementBase
+    , IPointerClickHandler
+{
+    void Update()
+    {
+        if (UIHelper.GetKeyDown(KeyCode.Space))
+        {
+            Accept();
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Accept();
+    }
+
+    private void Accept()
+    {
+        AudioSFXController.Instance.PlaySFX(AudioHelper.UIClick);
+
+        UIConfirmationController.Instance.Accept();
+    }
+
+    public override void HandleTooltip()
+    {
+        //Left as stub
+    }
+}
