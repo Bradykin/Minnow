d6d3fed81a0f23b3da56dfb903fd2e952cf75800
nickmartino3 <nickmartino2011@hotmail.com>
2021-01-02 20:49:38 -0500

    Bird nerfs

--- Assets/Scripts/Gameplay/Content/Units/Enemy/ContentAngryBirdEnemy.cs
+++ Assets/Scripts/Gameplay/Content/Units/Enemy/ContentAngryBirdEnemy.cs
@@ -9,8 +9,8 @@ public class ContentAngryBirdEnemy : GameEnemyUnit
     {
         m_maxHealth = 5;
         m_maxStamina = 5;
-        m_staminaRegen = 4;
-        m_power = 5;
+        m_staminaRegen = 3;
+        m_power = 2;
         m_attackSFX = AudioHelper.BirdFlap;
 
         m_team = Team.Enemy;
