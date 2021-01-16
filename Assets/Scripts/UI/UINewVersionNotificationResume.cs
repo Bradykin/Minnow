f305fa4b260ec1c88992fc1b4fc75e9a11f01390
nickmartino3 <nickmartino2011@hotmail.com>
2021-01-16 16:40:29 -0500

    Add a way to close the menu

---
+++ Assets/Scripts/UI/UINewVersionNotificationResume.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UINewVersionNotificationResume : UIElementBase
+        , IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        AudioSFXController.Instance.PlaySFX(AudioHelper.UIClick);
+
+        UINewVersionNotificationController.Instance.Close();
+    }
+
+    public override void HandleTooltip()
+    {
+        //Left as stub
+    }
+}
